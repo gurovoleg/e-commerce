d82128bf6e4e1312e68bb0b6ee8b7120219c3d9d
gurovoleg <gogotheog@gmail.com>
2018-08-03 23:37:32 +0300

    adaptive

--- src/js/main.js
+++ src/js/main.js
@@ -1,30 +1,74 @@
 $(document).ready(function() {
 
-	$(".owl-carousel-newcomers").owlCarousel({
-		 // singleItem : true,
-	    items:4,
-	    slideSpeed : 800,
-	    paginationSpeed : 800,
-	    rewindSpeed : 1000,
-	    navigation : true,
-	    pagination : false,
-    	paginationNumbers: false,
-	    navigationText : ["",""],
-	    theme : "newcomers-slider-theme"
-	});
+	var owlNewcomers = $(".owl-carousel-newcomers");
 
-	$(".owl-carousel-menu").owlCarousel({
+	owlNewcomers.owlCarousel({
 		 // singleItem : true,
-	    items:4,
+	    items : 4, //4 items above 1000px browser width
+	    itemsDesktop : [1200,4], //4 items between 1200px and 993px
+	    itemsDesktopSmall : [992,3], // betweem 992px and 769px
+	    itemsTablet: [768,2], //2 items between 768 and 0
+	    // itemsMobile : [540,1] // itemsMobile disabled - inherit from itemsTablet option
+
 	    slideSpeed : 800,
 	    paginationSpeed : 800,
 	    rewindSpeed : 1000,
 	    navigation : false,
 	    pagination : false,
-	    autoPlay : true,
-	    stopOnHover : true,
-    	paginationNumbers: false
-	     // theme : "newcomers-slider-theme"
+    	paginationNumbers: false,
+	    navigationText : ["",""],
+	    // theme : "newcomers-slider-theme"
+	});
+
+	// Custom Navigation Events
+	$(".next").click(function(){
+		owlNewcomers.trigger('owl.next');
+	})
+	$(".prev").click(function(){
+		owlNewcomers.trigger('owl.prev');
+	})
+
+	//Menu carousel
+	$(".owl-carousel-menu").owlCarousel({
+		 // singleItem : true,
+	    items:4
+
+	});
+
+	var owlHeader = $(".owl-carousel-header");
+
+	owlHeader.owlCarousel({
+		singleItem : true,
+	    // items : 4, //4 items above 1000px browser width
+	    // itemsDesktop : [1200,4], //4 items between 1200px and 993px
+	    // itemsDesktopSmall : [992,3], // betweem 992px and 769px
+	    // itemsTablet: [768,2], //2 items between 768 and 0
+	    // // itemsMobile : [540,1] // itemsMobile disabled - inherit from itemsTablet option
+
+	    slideSpeed : 800,
+	    paginationSpeed : 800,
+	    rewindSpeed : 1000,
+	    navigation : false,
+	    pagination : false,
+    	paginationNumbers: false,
+	    navigationText : ["",""],
+	    // theme : "newcomers-slider-theme"
+	});
+
+	// Custom Navigation Events
+	$(".h-next").click(function(){
+		owlHeader.trigger('owl.next');
+	})
+	$(".h-prev").click(function(){
+		owlHeader.trigger('owl.prev');
+	})
+
+
+
+
+
+	$('.fa-star').on('click',function(){
+		$(this).toggleClass('favorite');
 	});
 	
 	
